c3b563281708f29eb85da676c2159ee7e69655b3
Khaled Garbaya <khaledgarbaya@gmail.com>
2019-12-14 17:04:40 +0100

    Load Timeline data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Link, Router } from '@reach/router'
 
+const Timeline = () => {
+  const [timeline, setTimeline] = useState([])
+
+  useEffect(() => {
+    fetch('https://k4d.dev/.netlify/functions/timeline')
+      .then(response => response.json())
+      .then(timelineData => setTimeline(timelineData))
+  }, [])
+
+  return (
+    <div>
+      <h1>Timeline</h1>
+      <ul>
+        {timeline.map(t => (
+          <li key={t.id}>
+            <div className="p-10 border border-gray-400 bg-white shadow">
+              <div className="flex items-center">
+                <img
+                  className="w-10 h-10 rounded-full mr-4"
+                  src={t.user.profileImageUrl}
+                  alt={t.user.name}
+                />
+                <div className="text-sm">
+                  <p className="text-gray-900 leading-none">
+                    {t.user.name}
+                  </p>
+                  <p className="text-gray-600">
+                    @{t.user.screenName}
+                  </p>
+                </div>
+              </div>
+              <div
+                dangerouslySetInnerHTML={{ __html: t.text }}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const Policy = () => <h1>Policy</h1>
+const About = () => <h1>About</h1>
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/timeline">Timeline</Link>{' '}
+        <Link to="/policy">Policy</Link>{' '}
+        <Link to="/about">About</Link>
+      </nav>
+      <Router>
+        <Timeline path="timeline" />
+        <Policy path="policy" />
+        <About path="about" />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
